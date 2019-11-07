dac0e7da02e6512b1acd5098b41fe6d96e09d33a
okanserdaroglu2@gmail.com <iserdaroglu@thy.com>
2019-11-07 13:24:30 +0300

    calculated in different planets

--- app/src/main/java/com/okanserdaroglu/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/okanserdaroglu/firstkotlinapp/MainActivity.kt
@@ -2,50 +2,92 @@ package com.okanserdaroglu.firstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private val KILO_TO_POUND = 2.2045
-    private val MARS = 0.38
-    private val POUND_TO_KG = 0.45359237
+    private var userKg : String = ""
+    private val constantKiloToPound : Double = 2.2045
+    private val constantMars : Double = 0.38
+    private val constantPoundToKg : Double = 0.45359237
+    private val constantVenus : Double = 0.91
+    private val constantJupiter : Double = 2.34
+    private var weightPound : Double = 0.0
+    private var weightInMars : Double = 0.0
+    private var weightInMarsKg : Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var userKg = editTextWeight.text
-
-        buttonCalculate.setOnClickListener {
-
-            var weightPound = kgToPound(userKg.toString().toDouble())
-            var weightInMars = weightPound * MARS
-            var weightInMarsKg = poundToKg(weightInMars)
-
-            textViewResult.text = weightInMarsKg.format(2).toString()
-
-        }
+        buttonCalculate.setOnClickListener(this)
+        checkBoxJupiter.setOnClickListener(this)
+        checkBoxMars.setOnClickListener(this)
+        checkBoxVenus.setOnClickListener(this)
 
     }
 
     private fun kgToPound(kg: Double): Double {
 
-        return kg * KILO_TO_POUND
+        return kg * constantKiloToPound
 
     }
 
     private fun poundToKg(pound: Double): Double {
 
-        return pound * POUND_TO_KG
+        return pound * constantPoundToKg
 
     }
 
-    private fun Double.format (number : Int) = java.lang.String.format("%.${number}f",this) /** extention function added for Double class */
+    private fun Double.format (number : Int) = java.lang.String.format("%.${number}f",this)
+    //extension function added for Double class
 
-    override fun onClick(p0: View?) {
+    override fun onClick(v: View?) {
+
+        if (!TextUtils.isEmpty(editTextWeight.text.toString())) {
+            userKg = editTextWeight.text.toString()
+            weightPound = kgToPound(userKg.toDouble())
+        } else {
+            return
+        }
+
+        when (v) {
+            is CheckBox -> { // check v is a checkBox
+
+                weightInPound(weightPound,v)
+
+            }
+            is Button -> { // check v is a button
+
+                weightInMars = weightPound * constantMars
+                weightInMarsKg = poundToKg(weightInMars)
+
+                textViewResult.text = weightInMarsKg.format(2).toString()
+
+            }
+        }
 
 
+    }
+
+    private fun weightInPound (pound : Double, checkBox: CheckBox){
+
+        var result = 0.0
+
+        when (checkBox.id){
+
+            R.id.checkBoxMars -> result = pound * constantMars
+            R.id.checkBoxVenus -> result = pound * constantVenus
+            R.id.checkBoxJupiter -> result = pound * constantJupiter
+
+        }
+
+        val resultToKg = poundToKg(result)
+        textViewResult.text = resultToKg.format(2).toString()
 
     }
 
